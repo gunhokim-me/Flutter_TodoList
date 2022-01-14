fe0ded8ea951afa279b303e86a8a30441650ee0c
gunhokim <moiraaa@naver.com>
2022-01-14 22:25:50 +0900

    remove ignored file

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.nunitoSansTextTheme(
+          Theme.of(context).textTheme,
+        )
+      ),
+      home: Scaffold(
+        body: Container(
+          child: Center(
+            child: Text("Hello World!",
+            style: TextStyle(
+              fontSize: 22.0
+            ),),
+          ),
+        ),
+      ),
+    );
+  }
+}
