092952578329c18aed739a7f21a0572efd42547c
gunhokim <moiraaa@naver.com>
2022-01-19 19:13:34 +0900

    Add taskPage

--- lib/screen/homepage.dart
+++ lib/screen/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_todo/screen/widgets.dart';
+import 'package:flutter_todo/screen/taskpage.dart';
+import 'package:flutter_todo/widgets.dart';
 
 class Homepage extends StatefulWidget {
 
@@ -16,7 +17,6 @@ class _HomepageState extends State<Homepage> {
           width: double.infinity,
           padding: EdgeInsets.symmetric(
               horizontal: 24.0,
-              vertical: 32.0
           ),
           color: Color(0xFFF6F6F6),
           child: Stack(
@@ -26,6 +26,7 @@ class _HomepageState extends State<Homepage> {
                 children: [
                   Container(
                     margin: EdgeInsets.only(
+                      top: 32.0,
                       bottom: 32.0
                     ),
                     child: Image(
@@ -34,26 +35,53 @@ class _HomepageState extends State<Homepage> {
                       ),
                     ),
                   ),
-                  TaskCardWidget(),
-                  TaskCardWidget(
-                    title: "Get Started!",
-                    desc: "hello World!!",
-                  ),
+                  Expanded(
+                    child: ScrollConfiguration(
+                      behavior: NoScrollBehavior(),
+                      child: ListView(
+                        children: [
+                          TaskCardWidget(),
+                          TaskCardWidget(
+                            title: "Get Started!",
+                            desc: "hello World!!",
+                          ),
+                          TaskCardWidget(),
+                          TaskCardWidget(),
+                        ],
+                      ),
+                    ),
+                  )
                 ],
               ),
               Positioned(
-                bottom: 0.0,
+                bottom: 24.0,
                 right: 0.0,
-                child: Container(
-                  width: 60.0,
-                  height: 60.0,
-                  decoration: BoxDecoration(
-                    color: Color(0xFF7349FE),
-                    borderRadius: BorderRadius.circular(20.0)
-                  ),
-                  child: Image(
-                    image: AssetImage(
-                      "assets/images/add_icon.png"
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => TaskPage()
+                      ),
+                    );
+                  },
+                  child: Container(
+                    width: 60.0,
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Color(0xFF7349FE), Color(0xFF643FDB)
+                        ],
+                        begin: Alignment(0.0, -1.0),
+                        end: Alignment(0.0, -1.0),
+                      ),
+                      borderRadius: BorderRadius.circular(20.0)
+                    ),
+                    child: Image(
+                      image: AssetImage(
+                        "assets/images/add_icon.png"
+                      ),
                     ),
                   ),
                 ),
